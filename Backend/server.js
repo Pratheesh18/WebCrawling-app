bbc1206ca0bab523d71ffd1a0c14eb3405cfa980
Pratheesh18 <pratheesh1218@gmail.com>
2024-05-30 12:09:36 +0530

    added the frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const fetchTopPosts = require('./crawler');
+const {fetchTopPosts} = require('./crawler');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors());
 
 app.get('/api/top-posts' , async (req,res) => {
     try{
         const topPosts = await fetchTopPosts();
+        console.log("Memes" , topPosts)
         res.json(topPosts);
     }catch(error){
         console.error("Error fetching" , error);
         res.status(500).json({error:'Failed to fetch the posts'});
     }
 });
-
 
 const PORT  = 5000;
 app.listen(PORT , () => {
---
+++ Frontend/reddit-app/src/components/MemePage.jsx
@@ -0,0 +1,59 @@
+import React , {useState , useEffect} from 'react';
+import axios from 'axios';
+import {jsPDF} from 'jspdf';
+
+
+
+const MemePage = () => {
+    const [topPosts , setTopPosts] = useState([]);
+
+
+    useEffect(() => {
+        fetchTopPosts();
+        console.log("Posts " , fetchTopPosts());
+    },[]);
+
+    const fetchTopPosts = async () => {
+        try{
+            const res = await axios.get('http://localhost:5000/api/top-posts');
+            console.log("memes" , res.data)
+            setTopPosts(res.data);
+        }catch(err){
+            console.error('Error fetching data ' , err);
+        }
+    };
+
+
+    const generatePdf = () => {
+        const doc = new jsPDF();
+        let yPos = 10;
+
+        doc.text("Top 20 trending memes " , 10,yPos);
+
+        topPosts.forEach((post,index) => {
+            ypos += 10;
+            doc.text(`${index + 1}. ${post.title}`, 10, yPos);
+            doc.text(`${post.url}`, 10, yPos + 5);
+        });
+
+        doc.save('Top_Memes_Report.pdf');
+    };
+
+
+    return(
+        <div>
+            <h2>  Top 20 Trending memes </h2>
+            <button onClick={generatePdf}> Generate PDF </button>
+            <ul>
+                {topPosts.map((meme,index) => (
+                    <li key={index}>
+                        <strong> {meme.title} </strong> - {meme.url}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+};
+
+
+export default MemePage;
