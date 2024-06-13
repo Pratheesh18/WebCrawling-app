e93c15afe37b5fa702554759260b1cada53070c9
Pratheesh18 <pratheesh1218@gmail.com>
2024-06-13 16:32:37 +0530

    created routes for registration and login

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -1,4 +1,61 @@
 const User = require('../Models/User');
 const generateToken = require('../Utils/generateToken');
 
-exports.
+exports.registerUser = async (req,res) => {
+    const {username , email , password} = req.body;
+
+    try{
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return res.status(400).json({message : 'User already exists'});
+        }
+        const user = await User.create({username , email,password});
+        res.status(201).json({
+            _id : user._id,
+            username : user.username,
+            email:user.email,
+            token : generateToken(user._id),
+        });
+        console.log(`Registration Successful for user : ${email}`);
+    }catch(err){
+        res.status(500).json({message : 'Server Error' , err});
+    }
+
+};
+
+exports.loginUser = async (req,res) => {
+    const {email , password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(user && (await user.matchPassword(password))){
+            res.json({
+                _id : user._id,
+                username : user.username,
+                email : user.email,
+                token : generateToken(user._id),
+            });
+        }else{
+            res.status(401).json({message : 'Invalid email or password'});
+        }
+        console.log(`Login successful for user : ${email}`);
+    }catch(err){
+        res.status(500).json({message : 'Server Error' , err});
+    }
+};
+
+
+exports.getUserProfile = async (req,res) => {
+    const user = await User.findById(req.user._id);
+    if(user){
+        res.json({
+            _id:user._id,
+            username : user.username,
+            email:user.email,
+        });
+    }else{
+        res.status(404).json({message : 'User not found'});
+
+    };
+
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const {fetchTopPosts} = require('./crawler');
+const authRoutes = require('./Routes/authRoutes')
 const cors = require('cors')
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth' , authRoutes)
 
 app.get('/api/top-posts' , async (req,res) => {
     try{
@@ -15,6 +21,16 @@ app.get('/api/top-posts' , async (req,res) => {
         res.status(500).json({error:'Failed to fetch the posts'});
     }
 });
+
+
+ mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+ })
+   .then(() => console.log('Mongo DB Connected'))
+   .catch(err => console.error('MongoDB connection error' , err));
+
+
 
 const PORT  = 5000;
 app.listen(PORT , () => {
